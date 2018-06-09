5e9b0b43eef93bfe960b6e6e7d9f65e666951422
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-06-09 22:40:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = 'ALED LE TOKEN';
+const token = 'NDU1MTA3MDg2NDE4ODM3NTIO.Df3LDw.ff3aJkYUJWooMsb2YZBZcl7sPzY';
 const fs = require("fs");
 const idSven = '152045576354004992'
 var cli = new Discord.Client({autoReconnect:true});
@@ -10,12 +10,11 @@ bot.commands = new Discord.Collection();
 bot.on("error", (e) => console.error(e));
 bot.on("warn", (e) => console.warn(e));
 bot.on("debug", (e) => console.info(e));
-bot.on('ready', function() {console.log(" McBully https://...")},
+bot.on('ready', function() {console.log(" McBully https://discordapp.com/oauth2/authorize?client_id=455107086418837524&scope=bot&permissions=104324160")},
 
 bot.on('ready',() => {
   bot.user.setGame(" bully Sven");
 }));
-bot.on('message', message => {
 
 
 
