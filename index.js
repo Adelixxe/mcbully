f9ddc9a1cf1b011f4250aad23efc79e9a6bd3270
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-06-09 21:29:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'ALED LE TOKEN';
 
+client.on('ready', () => {
+  console.log('Go bully Sven!');
+});
+
+
+
+
+
+
+
+client.login(token);
